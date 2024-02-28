adea894ab5014e344bcc57f612a11f8920625fec
Steve Earth <se435@drexel.edu>
2024-02-28 17:27:59 -0500

    added errLog

--- python-server/expression-tree/Parser.py
+++ python-server/expression-tree/Parser.py
@@ -1,5 +1,13 @@
-import re
+import string #no longer using re
+from typing import List #used to create a type hint for a list of strings
 
+#perhaps in future allow square brackets and braces. 
+whitespace = ["\n","\t","\r"," "] #permits linebreak and indents for conditionals. all become \s in pre-processing step
+arithSymbols = ["+","*","-"] # other math uses ascii, such as expt, quotient, remainder. Note: "/" not permitted
+openGroupSymb = ["(","[","{"] # needed to separate open from closed for more precision with error messaging
+closeGroupSymb = [")","]","}"] # possibly cond might be implemented one day with square brackets. presently, all these replaced by parens in pre-processing
+specialChars = ["#","?","\u03BB","'"] #hashtag for bools,? for predicate suffix,unicode is for lambda (currently not in language), final item is single quote
+AllowedChars = list(string.ascii_letters) + list(string.digits) + whitespace + arithSymbols + openGroupSymb + closeGroupSymb + specialChars
 class Node:
     def __init__(self, children=[], parent=None, data='', name=None, tokenType=None, debug=False):
         self.children = children
@@ -27,21 +35,40 @@ class Node:
             ans += ')'
         return ans
         
-
-def preProcess(inputString:str, debug=False) -> list:
-    inputString = inputString.lower()
+#errLog is a list of strings of error msgs that will be passed at each step of the tree-building process
+def preProcess(inputString:str, errLog:List[str]=None, debug=False) -> list: #None will generate a warning since it's not a list of strings
+    if errLog == None: #values assigned at func def, not each call, so need None vs []
+        errLog = []
+    #orig=inputString #saving original to refer to later, but might not be needed
+    # inputString = inputString.lower()  #decided to permit uppercase letters and make it case sensitive to allow M vs m. caution: now  "If" is not "if"inputString=inputString.replace("]",")").replace("[","(").replace("{","(").replace("}",")").replace("\t"," ").replace("\r"," ").replace("\n"," ").replace("("," ( ").replace(")"," ) ")
+    inputString = " ".join(inputString.split()) #this will remove consecutive spaces and also strip whitespace from front&back
+    if inputString == "": #needed to avoid an issue in checking first character as (
+        errLog.append("no input detected")
+        return [], errLog
+    # note that final replacement at end of next line attaches a \s to parens for list-splitting purposes
+    
     parenPairing = 0
-    for char in inputString:
+    for ind in range(len(inputString)): # needed to loop over index values rather than char to know if not at end
+        char = inputString[ind]
+        if char not in AllowedChars:
+            errLog.append(f"{char} not an allowed character")
         if char == '(':
             parenPairing += 1
         elif char == ')':
             parenPairing -= 1
-    if parenPairing == 0:
-        inputString = re.sub(r'\s+', ' ', inputString).replace("(", " ( ").replace(")", " ) ").split(' ')
-        inputString = [element for element in inputString if element != '']
-        return inputString
-    else:
-        return []
+        # the following conditionals refer to the general lists of chars in case later developers decide not to the the replacements
+        if char in openGroupSymb and parenPairing==1 and 0<ind<len(inputString)-1:  #i.e. parens balanced in the interior of the string
+            errLog.append("contains multiple independent subexpressions") #"(stuff)(stuff)".  need )( check to insure "34" doesn't trigger err
+        if char in whitespace and inputString[0] not in openGroupSymb:
+            errLog.append("multiple elements should be contained within a list")
+    if parenPairing < 0:
+        errLog.append("too many )")
+    elif parenPairing > 0:
+        errLog.append("too many (")
+    #else:  #I don't think this is needed anymore
+    #    inputString = re.sub(r'\s+', ' ', inputString).replace("(", " ( ").replace(")", " ) ").split(' ')
+    #    inputString = [element for element in inputString if element != ''] 
+    return inputString.split(),errLog
     
 def findMatchingParenthesis(tokenList, index):
     count = 1
@@ -53,10 +80,10 @@ def findMatchingParenthesis(tokenList, index):
         if count == 0:
             return i
 
-def buildTree(inputList:list[str], debug=False) -> list:
+def buildTree(inputList:list[str], errLog, debug=False) -> list:
     # if inputList == [], return the empty list
     if len(inputList) == 0:
-        return [] #steve made a comment
+        return [] 
     
     # we have something in inputList, create a Node
     node = Node([], debug=debug) # need [] inside Node init to ensure empty children list when created
@@ -67,7 +94,7 @@ def buildTree(inputList:list[str], debug=False) -> list:
 
         # create Node where Node.data is the literal and continue processing the rest of input
         return [node] + buildTree(inputList[1:len(inputList)], debug)
-    
+     
     if inputList[0] == '(' and inputList[1] == ')':
         
         # special case for the empty list '()', just modify Node.data == '()'
@@ -89,5 +116,3 @@ def buildTree(inputList:list[str], debug=False) -> list:
 
     # continue processing the rest of input
     return [node] + buildTree(inputList[matchIndex+1:len(inputList)], debug)
-        
-    
--- python-server/expression-tree/test.py
+++ python-server/expression-tree/test.py
@@ -1,72 +1,43 @@
-import Parser
-import Labeler
-# This should probably be removed from the git repo later, but is used as a scratch pad for
-# testing functionality. Later we will add unit tests.
+import Parser, Labeler
 
-# def defineTFL():
-#     # Define the individual types of tokens that can be used to construct an expression
-#     # These may be individual characters, but may be multiple characters each
-#     tVar = TokenType('Variable',r'([A-Z])',r'\1',True)
-#     tTrue = TokenType('True',r'True|TRUE','True',True)
-#     tFalse = TokenType('False',r'False|FALSE','False',True)
-#     tAnd = TokenType('And',r'([&^∧])','∧')
-#     tOr = TokenType('Or',r'([v\|∨])','∨')
-#     tNot = TokenType('Not',r'([~-¬])','¬')
-#     tImplies = TokenType('Implies',r'(->|>|→)','→')
-#     tOpenParens = TokenType('Open Parenthesis',r'(\()','(')
-#     tClosedParens = TokenType('Closed Parenthesis',r'(\))',')')
-#     tWhitespace = TokenType('Whitespace',r'\s+',' ')
-#     tBicond = TokenType('Biconditional',r'(<->|↔)','↔')
-#     tContradiction = TokenType('Contradiction',r'!','⊥',True)
-#     tokenList = TList[TokenType](TokenType,[tVar,tTrue,tFalse,tAnd,tOr,tNot,tImplies,tOpenParens,tClosedParens,
-#                                     tWhitespace,tBicond,tContradiction])
-
-#     # Define the ExpressionTypes that represent how each type of expression is constructed from tokens or
-#     # subexpressions. The 'Generic' ExpressionType is used to match against anything that is already an
-#     # expression. Booleans for now don't work as a group of Tokens because ORing functionality isn't implemented 
-#     # yet.
-#     tExpression = TokenType.getGeneric('Expression')
-#     eVar = ExpressionIdentifier('Variable',TList[TokenType](TokenType, [tVar]))
-#     # eBoolean = ExpressionType('Boolean',TList(TokenType,[tTrue|tFalse|tContradiction]))
-#     eAnd = ExpressionIdentifier('And',TList[TokenType](TokenType, [tExpression, tAnd, tExpression]))
-#     eOr = ExpressionIdentifier('Or',TList[TokenType](TokenType, [tExpression, tOr, tExpression]))
-#     eNot = ExpressionIdentifier('Not',TList[TokenType](TokenType, [tNot, tExpression]))
-#     # The eParens ExpressionType simply wraps a given expression in parentheses and any validity and identity
-#     # checks gets passed through the subexpression
-#     eParens = ExpressionIdentifier('Parentheses',TList[TokenType](TokenType, [tOpenParens, tExpression, tClosedParens]))
-#     eImplies = ExpressionIdentifier('Implication',TList[TokenType](TokenType, [tExpression, tImplies, tExpression]))
-#     eBicond = ExpressionIdentifier('Biconditional',TList[TokenType](TokenType, [tExpression, tBicond, tExpression]))
-#     eContradiction = ExpressionIdentifier('Contradiction',TList[TokenType](TokenType, [tContradiction]))
-#     expressionList = TList[ExpressionIdentifier](ExpressionIdentifier, [eVar,eAnd,eOr,eNot,eParens,eImplies,eBicond,eContradiction])
-#     return tokenList, expressionList
-
-
-
-# This will be the general workflow for defining a proof engine (at least for parsing)
-
-test_strings_tfl = [
-    '(A&B)vC', # A,B,C,A&B,(A&B),(A&B)vC
-    # '(AvB)->D', # A,B,D,AvB,(AvB),(AvB)->D
-    # 'Av(B->C)', # A,B,C,B->C,(B->C),Av(B->C)
-    # 'AvB->C', # This should fail but currently parses as A,B,C,AvB,AvB->C
-    # '((A->B)&(B<->C))->D', #A,B,B,C,D,A->B,(A->B),B<->C,(B<->C),(A->B)&(B<->C),((A->B)&(B<->C)),((A->B)&(B<->C))->D
-] 
-# ER isn't ready yet but should in theory be handled similarly
-test_strings_er = [
+test_strings_ok = [
     '(λ (n) (if (zero? n) 1 (* n (fact (- n 1)))))',
-    
     '(name?)', # test for variables
     '(#t)', # test for booleans
     '(3 4)', # test for numbers
+    '(34 #t)',
+    '34',
     '()', # test for the empty list
-    '(#t #T)',
     '(#t (#t))',
-    '#t'
+    '#t',
+    '((#t)#t)', #nested list with optional whitespace missing
+    '((#t) #t)', #nested list with whitespace
+    '((if #t + *) 3 4)', #NOTE: functions as terminal used to always be tricky in prior versions
+    '(t)' #would fail if only testing booleans, but this could be a defined function with no inputs
 ]
-for test in test_strings_er:
-    print(f"'{test}'")
+
+test_strings_err = [
+    '(#t #T)', #this wont be caught as an error until the udf naming restrictions get implemented
+    '(+ 2 #t)', #this won't get caught as an error until types are implemented
+    '(+ 2 3 4)', # this won't get caught as an error type inputs are checked (too many)
+    '(+ 2)',  # too few inputs, also won't get caught yet
+    '#t #t',
+    '3 4',
+    '',
+    '((#t)',
+    '(#t) (#t)',
+    '(#t)(#t)',
+    '(#t))',
+    '($))'
+]
+
+for test in test_strings_ok+test_strings_err:
+    print(f"input = {test}")
     debugStatus = False
-    exprList = Parser.preProcess(test,debug=debugStatus)
-    exprTree = Parser.buildTree(exprList, debug=debugStatus)[0]
-    labeledTree = Labeler.labelTree(exprTree)
-    print(labeledTree)
+    exprList,errLog = Parser.preProcess(test,errLog=[],debug=debugStatus)
+    if not errLog:
+        exprTree = Parser.buildTree(exprList, errLog, debug=debugStatus)[0]
+        labeledTree = Labeler.labelTree(exprTree)
+        print(labeledTree)
+    else:
+        print(errLog)
