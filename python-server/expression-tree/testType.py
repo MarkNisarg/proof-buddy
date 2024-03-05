190626abe7211952372fbdcf11f4ea3392b7ae6c
Steve Earth <se435@drexel.edu>
2024-03-04 19:07:42 -0500

    helper functions for refactored Types

--- python-server/expression-tree/testType.py
+++ python-server/expression-tree/testType.py
@@ -1,4 +1,4 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 from enum import Enum
 
 class coreType(Enum):
@@ -10,35 +10,55 @@ class coreType(Enum):
     ANY = 'ANY'
     ERROR = 'ERROR'
     NONE = 'NONE'
+    FUNCTION = 'FUNCTION' # this is only here for getType and should never be used directly
 
     def __str__(self):
         return self.value
 
 RacType = Union[Tuple[None, coreType], Tuple[Tuple['RacType', ...], coreType]]
 
-# Standalone recurse function
+# pretty print recursive function (commas between domain elements, with > separating out range)
 def helpPrint(items):
-    if isinstance(items, RacTypeClass):
+    if isinstance(items, RacType):
         return helpPrint(items.value)
     elif isinstance(items, tuple) and items[0] is None:
         return str(items[1])
     elif isinstance(items, tuple):
-        # Extract and process the first element
         domain = ', '.join(helpPrint(item) for item in items[0] if item is not None)
-        # Process the second element
         range = helpPrint(items[1])
         return f"({domain}) > {range}"
     return str(items)
 
-class RacTypeClass:
+class RacType:
     def __init__(self, value: RacType):
         self.value = value
 
     def __str__(self):
         return helpPrint(self.value)
+class TypeList:
+    def __init__(self,value:List[RacType]):
+        self.value = value
+    def __str__(self):
+        return '[' + ', '.join(str(x) for x in self.value) + ']'
+    
+def getType(T:RacType):
+    if T.value[0]==None:
+        return T.value[1]
+    return coreType.FUNCTION
 
-# Example usage
-ex1 = RacTypeClass((((((None, coreType.LIST), (None, coreType.BOOL)), coreType.INT), \
-                     (((None, coreType.INT), (None, coreType.LIST)), coreType.BOOL)), \
-                    coreType.LIST))
-print(ex1) # "((LIST, BOOL) > INT, (INT, LIST) > BOOL) > LIST"
+def getDomain(T:RacType):
+    if getType(T)!=coreType.FUNCTION:
+        return TypeList([RacType(coreType.ERROR)])
+    return TypeList([RacType(x) for x in T.value[0]])
+
+def getRange(T:RacType):
+    if getType(T)!=coreType.FUNCTION:
+        return RacType(coreType.ERROR)
+    return RacType(T.value[1])
+
+# unit tests
+tests = [RacType((None, coreType.INT)), RacType((((((None, coreType.LIST), (None, coreType.BOOL)), \
+        coreType.INT), (((None, coreType.INT), (None, coreType.LIST)), coreType.BOOL)), coreType.LIST))]
+
+for t in tests:
+    print(f"this expression has type {getType(t)} with domainList = {getDomain(t)} and range = {getRange(t)}")
