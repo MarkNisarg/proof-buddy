e3d54d6f3b23da72cc37162f1ee9f6e9292ca87d
connor <secen.0609@gmail.com>
2024-03-14 20:38:27 -0400

    Added ability to test multiple rules in the same list

--- python-server/expression_tree/testApplyRule.py
+++ python-server/expression_tree/testApplyRule.py
@@ -1,34 +1,34 @@
 import recParser, Labeler, typeFile, Decorator
 
 test_strings_ruleIf= [
-    "(cons (first '(1 2 3)) (rest '(1 2 3)))",
-    "(first (cons 3 '(1 2)))",
-    "(rest (cons 3 '(1 2)))",
-    "(if #t 4 5)", #expected 4
-    "(if #f x y)", #expected y
-    "(if asd a a)", #expected a
-    "(if #f b (c d e f g))", #expected (c d e f g)
-    "(if x (abc) (abc) )", #expected (abc) 
-    "(if #t (a b)(a))", #expected (a b)
-    "(if #t (if #f x y) (z))", #expected (if #f x y)
-    "(if a b c)", #expected (if a b c) no valid change to make based on rules
-    "(if a (+ x (y (z z))) (+ x (y (z z))))", #expected (+ x (y (z z)))
-    "(if a (+ x (y (z z))) (+ x (b (z z))))", #expected no valid change
-    "(fi #t x y)", #expected invalid
-    "if #t x y", #expected invalid - should it give x? without () there are no child nodes so it doesnt read as a valid tree, is it supposed to?
-    "()", #expected invalid
-    "(if )", #expected invalid
-    "(if #t b c d e)", #expected invalid
+    ("cons", "(cons (first '(1 2 3)) (rest '(1 2 3)))"),
+    ("first", "(first (cons 3 '(1 2)))"),
+    ("rest", "(rest (cons 3 '(1 2)))"),
+    ("if", "(if #t 4 5)"), #expected 4
+    ("if", "(if #f x y)"), #expected y
+    ("if", "(if asd a a)"), #expected a
+    ("if", "(if #f b (c d e f g))"), #expected (c d e f g)
+    ("if", "(if x (abc) (abc) )"), #expected (abc) 
+    ("if", "(if #t (a b)(a))"), #expected (a b)
+    ("if", "(if #t (if #f x y) (z))"), #expected (if #f x y)
+    ("if", "(if a b c)"), #expected (if a b c) no valid change to make based on rules
+    ("if", "(if a (+ x (y (z z))) (+ x (y (z z))))"), #expected (+ x (y (z z)))
+    ("if", "(if a (+ x (y (z z))) (+ x (b (z z))))"), #expected no valid change
+    ("if", "(fi #t x y)"), #expected invalid
+    ("if", "if #t x y"), #expected invalid - should it give x? without () there are no child nodes so it doesnt read as a valid tree, is it supposed to?
+    ("if", "()"), #expected invalid
+    ("if", "(if )"), #expected invalid
+    ("if", "(if #t b c d e)"), #expected invalid
 ]
 
 print("\napplyRule testing:\n")
-for i in test_strings_ruleIf:
-    print("input =", i)
-    exprList,errLog = recParser.preProcess(i,errLog=[])
+for rule, expr in test_strings_ruleIf:
+    print("input =", expr)
+    exprList,errLog = recParser.preProcess(expr,errLog=[])
     exprTree = recParser.buildTree(exprList,)[0] # might not need to pass errLog
     labeledTree = Labeler.labelTree(exprTree)
     decTree, errLog = Decorator.decorateTree(labeledTree,errLog)
     errLog = Decorator.remTemps(decTree, errLog)
     decTree, errLog = Decorator.checkFunctions(decTree,errLog)
-    errLog = decTree.applyRule('cons', errLog)
+    errLog = decTree.applyRule(rule, errLog)
     print("after rule =", decTree)
